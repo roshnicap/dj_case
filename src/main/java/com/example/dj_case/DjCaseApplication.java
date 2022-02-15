27790da1505a456aab68c006b329f2081d968bc5
Udhaya <roshni.udhaya@capgemini.com>
2022-02-15 13:54:24 +0100

    Initial commit

---
+++ src/main/java/com/example/dj_case/DjCaseApplication.java
@@ -0,0 +1,14 @@
+package com.example.dj_case;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DjCaseApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/test/java/com/example/dj_case/DjCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dj_case;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
